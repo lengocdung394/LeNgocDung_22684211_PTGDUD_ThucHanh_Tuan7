1824e4e32c36b6a0588275d85f08911a958da6e4
H82M19\Student <Student>
2025-04-02 11:29:08 +0700

    layout

---
+++ dung_22684211/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+
+
+    <div style={{ marginLeft: "150px", display: "flex" }}>
+
+      <div id="conponent" style={{ display: 'flex', height: "800px", width: "1200px", margin: "10px" }}>
+        <div id="left" style={{ width: "200px" }}>
+          <h2 style={{ padding: 0, margin: 0 }}>Logo </h2>
+          <ul style={{ margin: "none", padding: "0" }}>
+
+            <li style={{ listStyle: "none", marginBottom: "20px", padding: "5px", backgroundColor: "lightblue", borderRadius: "3px", color: "black" }}>DashBoard</li>
+            <li style={{ listStyle: "none", marginBottom: "20px", padding: "5px", borderRadius: "3px" }}>Project</li>
+            <li style={{ listStyle: "none", marginBottom: "20px", padding: "5px", borderRadius: "3px" }}>Teams</li>
+            <li style={{ listStyle: "none", marginBottom: "20px", padding: "5px", borderRadius: "3px" }}>Analytics</li>
+            <li style={{ listStyle: "none", marginBottom: "20px", padding: "5px", borderRadius: "3px" }}>Messages</li>
+            <li style={{ listStyle: "none", marginBottom: "20px", padding: "5px", borderRadius: "3px" }}>Integrations</li>
+          </ul>
+
+          <img src="" alt="" />
+
+        </div>
+        <div id="right" style={{ width: "900px", paddingLeft:"20px" }}>
+
+          <div style={{ height: "100px", display: "flex" }}>
+            <h2 style={{ margin: "0", color: "pink" }}>DashBoard</h2>
+            <input type="text" name="" id=""  style={{ width: "200px", height: "25px", marginLeft: "200px" }} />
+
+
+          </div>
+          <div style={{ height: "300px" }}>
+
+            <h2>Overview</h2>
+
+            <div style={{ display: "flex" }}>
+
+              <div style={{ flex: 1, border: "1px solid white", borderRadius: "5px", margin: "5px", height: "150px" }}>1</div>
+              <div style={{ flex: 1, border: "1px solid white", borderRadius: "5px", margin: "5px", height: "150px" }}>2</div>
+              <div style={{ flex: 1, border: "1px solid white", borderRadius: "5px", margin: "5px", height: "150px" }}>3</div>
+            </div>
+
+          </div>
+          <div style={{ height: "400px" }}>
+
+            <table>
+
+
+
+
+
+            </table>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default App
