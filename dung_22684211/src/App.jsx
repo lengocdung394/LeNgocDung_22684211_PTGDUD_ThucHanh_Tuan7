27c68305cba51c575d113adb54fd7a28e4873e70
Co Phuong Hang <Dungheongunhubo>
2025-04-04 22:44:25 +0700

    data table

--- dung_22684211/src/App.jsx
+++ dung_22684211/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, use } from 'react'
 
 
 function App() {
 
   const [data, setData] = useState([])
+  const [datatable, setDataTable] = useState([])
+  const displayedData = datatable.slice(0, 6); // Hiển thị 10 dòng đầu tiên
   useEffect(() => {
     fetch('https://67c7c637c19eb8753e7ab0ce.mockapi.io/Overview')
       .then(response => response.json())
       .then(data => setData(data))
-  },[])
+  }, [])
   const colors = ["#FFB6C1", "#E0F7FA", "#FFD1DC"]; // Danh sách màu sắc
+  useEffect(() => {
+    fetch('https://67e369142ae442db76d0029b.mockapi.io/dttb')
+      .then(response => response.json())
+      .then(data => setDataTable(data))
+  }, [])
   return (
 
 
     <div style={{ marginLeft: "150px", display: "flex" }}>
 
-      <div id="conponent" style={{ display: 'flex', height: "800px", width: "1200px", margin: "10px" }}>
+      <div id="conponent" style={{ display: 'flex', height: "900px", width: "1300px", margin: "10px" }}>
         <div id="left" style={{ width: "200px", margin: "10px", border: "5px solid lightblue", borderRadius: "5px", backgroundColor: "white" }}>
           <h2 style={{ padding: 0, margin: 0 }}>Logo </h2>
           <ul style={{ margin: "none", padding: "0" }}>
@@ -28,35 +35,35 @@ function App() {
             <li style={{ listStyle: "none", marginBottom: "20px", padding: "5px", borderRadius: "3px" }}>Integrations</li>
           </ul>
 
-          
+
 
         </div>
-        <div id="right" style={{ width: "900px", paddingLeft: "20px", margin: "10px", border: "5px solid lightblue", borderRadius: "5px", backgroundColor: "white" }}>
+        <div id="right" style={{ width: "1000px", paddingLeft: "20px", margin: "10px", border: "5px solid lightblue", borderRadius: "5px", backgroundColor: "white" }}>
 
-          <div style={{ height: "70px", display: "flex", borderBottom: "5px solid lightblue", paddingTop:"15px" }}>
+          <div style={{ height: "70px", display: "flex", borderBottom: "5px solid lightblue", paddingTop: "15px" }}>
             <h2 style={{ margin: "0", color: "pink" }}>DashBoard</h2>
-            <input type="text" name="" id="" style={{ width: "220px", height: "25px", marginLeft: "410px",borderRadius: "5px" }} />
-            <img src="../image/bell.png" alt="" style={{width: "25px", height: "25px", marginLeft: "20px", marginRight: "20px"}} />
-            <h3 style={{margin: "0px"}}>?</h3>
-            <img src="../image/account.jpg" alt="" style={{width: "32px", height: "32px", borderRadius: "20px", marginLeft:"20px"}}/>
+            <input type="text" name="" id="" style={{ width: "220px", height: "25px", marginLeft: "410px", borderRadius: "5px" }} />
+            <img src="../image/bell.png" alt="" style={{ width: "25px", height: "25px", marginLeft: "20px", marginRight: "20px" }} />
+            <h3 style={{ margin: "0px" }}>?</h3>
+            <img src="../image/account.jpg" alt="" style={{ width: "32px", height: "32px", borderRadius: "20px", marginLeft: "20px" }} />
           </div>
           <div style={{ height: "300px", borderBottom: "5px solid lightblue" }}>
 
-            <h2 style={{color:"black"}}>Overview</h2>
+            <h2 style={{ color: "black" }}>Overview</h2>
 
-            <div style={{ display: "flex", padding:"0px" }}>
+            <div style={{ display: "flex", padding: "0px" }}>
               {
                 data.map((item, index) => {
                   const backgroundColor = colors[index % colors.length];
                   return (
-                    <div key={index} style={{ backgroundColor, width: "200px", margin: "10px", display: "flex", flexDirection: "column", width: "300px", height: "150px", borderRadius: "5px" , padding: "10px"}}>
+                    <div key={index} style={{ backgroundColor, width: "200px", margin: "10px", display: "flex", flexDirection: "column", width: "300px", height: "150px", borderRadius: "5px", padding: "10px" }}>
                       <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0px", height: "40px" }}>
                         <h3>{item.title}</h3>
-                        <div style={{width: "20px", height: "20px", border: "2px solid black", borderRadius: "7px", padding: "5px"}} dangerouslySetInnerHTML={{ __html: item.icon }} />
+                        <div style={{ width: "20px", height: "20px", border: "2px solid black", borderRadius: "7px", padding: "5px" }} dangerouslySetInnerHTML={{ __html: item.icon }} />
                       </div>
-                      
-                      <h2 style={{padding: "0px", margin: "0px", }}>${item.number}</h2>
-                      <div style={{display: "flex"}}> <p style={{color: "green"}}>{item.percent}%</p><p style={{marginLeft:"5px"}}>  period of change</p></div>
+
+                      <h2 style={{ padding: "0px", margin: "0px", }}>${item.number}</h2>
+                      <div style={{ display: "flex" }}> <p style={{ color: "green" }}>{item.percent}%</p><p style={{ marginLeft: "5px" }}>  period of change</p></div>
                     </div>
                   )
                 })
@@ -65,14 +72,36 @@ function App() {
             </div>
 
           </div>
-          <div style={{ height: "400px" }}>
+          <div style={{ height: "500px" , marginRight: "15px"}}>
 
-            <table>
-
-
-
-
-
+            <table style={{ width: "100%", borderCollapse: "collapse"}}>
+              <thead>
+                <tr>
+                  <th style={{ border: "1px solid #ddd", padding: "8px" }}><input type='checkbox' ></input></th>
+                  <th style={{ border: "1px solid #ddd", padding: "8px" }}>Customer Name</th>
+                  <th style={{ border: "1px solid #ddd", padding: "8px" }}>Company</th>
+                  <th style={{ border: "1px solid #ddd", padding: "8px" }}>Order Value</th>
+                  <th style={{ border: "1px solid #ddd", padding: "8px" }}>Order Date</th>
+                  <th style={{ border: "1px solid #ddd", padding: "8px" }}>Status</th>
+                  <th style={{ border: "1px solid #ddd", padding: "8px", width:"50px" }}></th>
+                </tr>
+              </thead>
+              <tbody>
+                {displayedData.map((item, index) => (
+                  
+                  <tr key={index} style={{ border: "1px solid #ddd", padding: "8px" }}>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                      <input type="checkbox" />
+                    </td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>{item.name}</td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>{item.company}</td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>{item.orderValue}</td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>{item.orderDate}</td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>{item.status}</td>
+                    <td style={{ border: "1px solid #ddd", padding: "8px" }}>{<img src='../image/edit.png' style={{width: "25px", height:"25px"}}></img>}</td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
 
           </div>
